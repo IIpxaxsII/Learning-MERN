d78d8955a14db45b0fd942ac8621a2ebb172c20a
IIpxrxs <bindraparas1322@gmail.com>
2025-07-07 21:41:23 +0530

    First mern commit

---
+++ first.js
@@ -0,0 +1,31 @@
+// console.log("hello");
+
+// const PI = 3.14;
+// let name = paras;
+// var a = 10;
+ 
+
+// const product = {
+//      name: "Ball pen",
+//      rating:4,
+//      offer:5,
+//      price:270,
+//      isDeal: true
+
+
+// };
+
+// console.log(product);
+
+
+ const profile = {
+     name: "Shradha Khapra",
+     BigInt: 569,
+     following: 5,
+     description: "Apna college",
+     isfollowing: false
+
+
+};
+
+console.log(profile);
