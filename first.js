aa1440c954e778237d7719b4bfde03db761228b8
IIpxrxs <bindraparas1322@gmail.com>
2025-07-12 23:15:19 +0530

    ..

--- first.js
+++ first.js
@@ -373,8 +373,9 @@ let promise = getPromise =()=>{
 let promise2 = getPromise()
     promise2.then(()=>{
         console.log("success")})
-*/
 
+
+//promise chaining
 function asyncFun1(){
     return new Promise((resolve,reject)=>{
 
@@ -397,3 +398,27 @@ asyncFun1().then((res)=>{
     asyncFun2().then((res)=>{})
     
 })
+        */
+//async await
+function getData(data){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data: ",data)
+            resolve("success")
+        },1000)
+    })
+}
+
+async function printData(){
+    console.log("getting data...")
+    await getData(1)
+    await getData(2)
+    await getData(3)
+    await getData(4)
+    await getData(5)
+    await getData(6)
+    await getData(7)
+    await getData(8)
+
+}
+printData()
